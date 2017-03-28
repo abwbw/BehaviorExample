16bec75c0059ff0e3f276b7de81498a128220c7f
王滨伟 <binwei_wang@kingdee.com>
2017-03-28 22:04:08 +0800

    third commit

--- app/src/main/java/com/example/abwbw/myapplication/PullDownBehavior.java
+++ app/src/main/java/com/example/abwbw/myapplication/PullDownBehavior.java
@@ -1,18 +1,14 @@
 package com.example.abwbw.myapplication;
 
 import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Rect;
 import android.support.design.widget.CoordinatorLayout;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.animation.AccelerateInterpolator;
 import android.view.animation.DecelerateInterpolator;
 import android.view.animation.TranslateAnimation;
 
@@ -23,16 +19,9 @@ import android.view.animation.TranslateAnimation;
  */
 
 public class PullDownBehavior extends CoordinatorLayout.Behavior<View> {
-    private static final int STATE_PULL = 1;
-    private static final int STATE_NORMAL = 2;
-    private static final int STATE_SHRINK = 3;
-
-    private int state = STATE_NORMAL;
-
     private Rect normal = new Rect();// 矩形空白
-    private boolean needShrink;
-    private int originHeight = -1;
-    private Animator animator;
+
+    private View target;
 
     public PullDownBehavior(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -42,53 +31,35 @@ public class PullDownBehavior extends CoordinatorLayout.Behavior<View> {
     public void onNestedScroll(CoordinatorLayout coordinatorLayout, View child, View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
         final View headView = coordinatorLayout.findViewById(R.id.head_nsv);
         Log.i("abwbw56","onNestedScroll :" + dyConsumed + " " + dyUnconsumed);
-
-//        if(isNeedAnimation()){
-//            translateY(child, dyConsumed);
-//        } else
-//
-//        if(needShrink){
-//            Log.i("abwbw56","need shrink");
-////            increateViewHeight(headView, dyUnconsumed);
-////            animation2(headView);
-////            translateY(child, dyConsumed);
-//        } else
-
-        if(isOverHeight(child) && !normal.isEmpty()){
-            child.layout(normal.left, normal.top, normal.right, normal.bottom);
-        }
-
+        this.target = target;
+        fixHeadLoaction(child);
 
         if(isNeedAnimation() && !isOverHeight(child) && headView == target){
             Log.i("abwbw56","need pull down isNeedAnimation");
-            translateY(child, dyUnconsumed);
-        } else
-            if(needPullDown(dyUnconsumed)){
-//            animation3(headView);
+            layout(child, dyUnconsumed);
+        }  else if(needPullDown(dyUnconsumed)){
             // 执行下拉操作
             Log.i("abwbw56","need pull down");
-            translateY(child, dyUnconsumed);
-        } else {
-//            super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
+            layout(child, dyUnconsumed);
+        }  else
+
+        if(isNeedAnimation() && !isOverHeight(child) && target instanceof RecyclerView) {
+            Log.i("abwbw56","need pull down isNeedAnimation RecyclerView");
+            layout(child, dyConsumed);
+            ((RecyclerView) target).requestDisallowInterceptTouchEvent(false);
+        }else {
+            super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
+        }
+    }
+
+    private void fixHeadLoaction(View childView){
+        if(isOverHeight(childView) && !normal.isEmpty()){
+            childView.layout(normal.left, normal.top, normal.right, normal.bottom);
         }
     }
 
     private boolean isOverHeight(View childView){
         return childView.getTop() <= 0;
-    }
-
-    @Override
-    public void onNestedPreScroll(CoordinatorLayout coordinatorLayout, View child, View target, int dx, int dy, int[] consumed) {
-        if(needShrink || needShrink(target, dy)){
-//            if(target instanceof RecyclerView){
-//                Log.i("abwbw56","onNestedPreScroll RecyclerView " + ((RecyclerView)target).isNestedScrollingEnabled());
-//                ((RecyclerView)target).setNestedScrollingEnabled(false);
-//            }
-            needShrink = true;
-        } else {
-            super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed);
-            needShrink = false;
-        }
     }
 
     @Override
@@ -107,24 +78,10 @@ public class PullDownBehavior extends CoordinatorLayout.Behavior<View> {
         if(isNeedAnimation()){
             animation(child);
         }
-
-        needShrink = false;
+        this.target = null;
     }
 
-    private boolean needShrink(View target, int dyUnconsumed){
-        if(target instanceof RecyclerView){
-
-            boolean canUp = ((RecyclerView) target).canScrollVertically(-1);
-            if( !canUp && dyUnconsumed > 0){
-                Log.i("abwbw56","need shrink 2 : dyUnconsumed " + dyUnconsumed);
-                return true;
-
-            }
-        }
-        return false;
-    }
-
-    public void translateY(View child, int y){
+    public void layout(View child, int y){
         if (normal.isEmpty()) {
             // 填充矩形，目的：就是告诉this:我现在已经有了，你松开的时候记得要执行回归动画.
             normal.set(child.getLeft(), child.getTop(),
@@ -149,65 +106,6 @@ public class PullDownBehavior extends CoordinatorLayout.Behavior<View> {
         // 设置回到正常的布局位置
         child.layout(normal.left, normal.top, normal.right, normal.bottom);
         normal.setEmpty();// 清空矩形
-    }
-
-    private void updateViewHeight(View child, int height){
-        final ViewGroup.LayoutParams lp = child.getLayoutParams();
-        lp.height = height;
-        child.setLayoutParams(lp);
-    }
-
-    private void increateViewHeight(View child, int height){
-        final ViewGroup.LayoutParams lp = child.getLayoutParams();
-        lp.height += height;
-        child.setLayoutParams(lp);
-    }
-
-    public void animation2(final View child) {
-        if(originHeight == -1) {
-            if(animator != null && (animator.isStarted() || animator.isRunning())){
-                animator.end();
-            }
-            originHeight = child.getHeight();
-            ValueAnimator animator = ValueAnimator.ofInt(child.getHeight(), 0);
-            animator.setDuration(300);
-            animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-                @Override
-                public void onAnimationUpdate(ValueAnimator animation) {
-                    updateViewHeight(child, (Integer) animation.getAnimatedValue());
-                }
-            });
-
-            animator.start();
-            this.animator = animator;
-        }
-        // 设置回到正常的布局位置
-
-//        normal.setEmpty();// 清空矩形
-    }
-
-    public void animation3(final View child){
-        if(originHeight != -1) {
-            if(animator != null && (animator.isStarted() || animator.isRunning())){
-                animator.end();
-            }
-            ValueAnimator animator = ValueAnimator.ofInt(0, originHeight);
-            animator.setDuration(300);
-            animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-                @Override
-                public void onAnimationUpdate(ValueAnimator animation) {
-                    updateViewHeight(child, (Integer) animation.getAnimatedValue());
-                }
-            });
-            animator.addListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    originHeight = -1;
-                }
-            });
-            animator.start();
-            this.animator = animator;
-        }
     }
 
     public boolean needPullDown(int dyUnConsumed){
--- app/src/main/java/com/example/abwbw/myapplication/ScrollingActivity.java
+++ app/src/main/java/com/example/abwbw/myapplication/ScrollingActivity.java
@@ -1,14 +1,20 @@
 package com.example.abwbw.myapplication;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 
 import java.util.ArrayList;
@@ -16,6 +22,16 @@ import java.util.List;
 
 public class ScrollingActivity extends AppCompatActivity {
     private ViewPager mContentVp;
+    private NestedScrollView mHeadNsv;
+
+    private ValueAnimator mHeadAnim;
+
+    private Toolbar mToolbar;
+
+    private int mHeadHeight;
+    private int mToolbarHeight;
+
+    private boolean mIsShrink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +43,97 @@ public class ScrollingActivity extends AppCompatActivity {
 
     private void findView(){
         mContentVp = (ViewPager) findViewById(R.id.content_vp);
+        mHeadNsv = (NestedScrollView) findViewById(R.id.head_nsv);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
     }
-
 
     private void initView(){
         mContentVp.setAdapter(new FragmentAdapter(getSupportFragmentManager()));
+        setSupportActionBar(mToolbar);
+        mHeadNsv.post(new Runnable() {
+            @Override
+            public void run() {
+                mHeadHeight = mHeadNsv.getHeight();
+            }
+        });
+
+        mToolbar.post(new Runnable() {
+            @Override
+            public void run() {
+                mToolbarHeight = mToolbar.getHeight();
+            }
+        });
+    }
+
+    private void shrinkHeadView(){
+        if(mIsShrink){
+            return;
+        }
+
+        if(mHeadAnim != null && (mHeadAnim.isRunning() || mHeadAnim.isStarted())){
+            mHeadAnim.cancel();
+        }
+
+        mHeadAnim = ValueAnimator.ofInt(mHeadHeight, mToolbarHeight);
+
+        mHeadAnim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                final ViewGroup.LayoutParams lp = mHeadNsv.getLayoutParams();
+                lp.height = (int) animation.getAnimatedValue();
+                mHeadNsv.setLayoutParams(lp);
+
+            }
+        });
+
+        mHeadAnim.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                mIsShrink = true;
+
+            }
+        });
+
+        mHeadAnim.setDuration(300);
+
+        mHeadAnim.start();
+
+
+    }
+
+    private void expandHeadView(){
+        if(!mIsShrink){
+            return;
+        }
+
+        if(mHeadAnim != null && (mHeadAnim.isRunning() || mHeadAnim.isStarted())){
+            mHeadAnim.cancel();
+        }
+
+        mHeadAnim = ValueAnimator.ofInt(mToolbarHeight, mHeadHeight);
+
+        mHeadAnim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                final ViewGroup.LayoutParams lp = mHeadNsv.getLayoutParams();
+                lp.height = (int) animation.getAnimatedValue();
+                mHeadNsv.setLayoutParams(lp);
+            }
+        });
+
+        mHeadAnim.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                mIsShrink = false;
+
+            }
+        });
+
+        mHeadAnim.setDuration(300);
+
+        mHeadAnim.start();
+
+
     }
 
     private class FragmentAdapter extends FragmentPagerAdapter implements ContentFragment.ScrollListener{
@@ -59,9 +161,17 @@ public class ScrollingActivity extends AppCompatActivity {
 
         @Override
         public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
-            Log.i("abwbw57","onScrolled:" + dy);
+            final LinearLayoutManager manager = (LinearLayoutManager) recyclerView.getLayoutManager();
 
+            final int fistPosition = manager.findFirstVisibleItemPosition();
 
+            if(fistPosition > 0){
+//                shrinkHeadView();
+                Log.i("abwbw58","shrinkHeadView");
+            } else if(fistPosition == 0){
+//                expandHeadView();
+                Log.i("abwbw58","expandHeadView");
+            }
         }
     }
 
